85b4ba7b0897c1f4942363225be577f7d51952c6
Ivo van Doesburg <ivo@fourohtwo.nl>
2015-10-14 16:22:02 +0200

    ignore multiline errors

--- sentrylogs/nginx.py
+++ sentrylogs/nginx.py
@@ -11,30 +11,34 @@ def nginx():
 
     for line in tailer.follow(open(filepath)):
 
-        # create the message
-        date_time_message, otherinfo = nginx_error_parser(line)
-        params = [date_time_message[2],
-                  date_time_message[0],
-                  date_time_message[1],
-                  otherinfo.get("request", "-"),
-                  otherinfo.get("referrer", "-"),
-                  otherinfo.get("server", "-"),
-                  otherinfo.get("client", "-"),
-                  otherinfo.get("host", "-"),
-                  otherinfo.get("upstream", "-")]
-        message ='%s' % date_time_message[2]
-        extended_message =  '%s\n'\
-                            'Date: %s\n'\
-                            'Time: %s\n'\
-                            'Request: %s\n'\
-                            'Referrer: %s\n'\
-                            'Server: %s\n'\
-                            'Client: %s\n'\
-                            'Host: %s\n'\
-                            'Upstream: %s\n'
-        site = otherinfo.get("referrer", "-")
+        try:
+            # create the message
+            date_time_message, otherinfo = nginx_error_parser(line)
+            params = [date_time_message[2],
+                      date_time_message[0],
+                      date_time_message[1],
+                      otherinfo.get("request", "-"),
+                      otherinfo.get("referrer", "-"),
+                      otherinfo.get("server", "-"),
+                      otherinfo.get("client", "-"),
+                      otherinfo.get("host", "-"),
+                      otherinfo.get("upstream", "-")]
+            message ='%s' % date_time_message[2]
+            extended_message =  '%s\n'\
+                                'Date: %s\n'\
+                                'Time: %s\n'\
+                                'Request: %s\n'\
+                                'Referrer: %s\n'\
+                                'Server: %s\n'\
+                                'Client: %s\n'\
+                                'Host: %s\n'\
+                                'Upstream: %s\n'
+            site = otherinfo.get("referrer", "-")
 
-        # send the message to sentry using Raven
-        send_message(message, extended_message, params, site, logger)
+            # send the message to sentry using Raven
+            send_message(message, extended_message, params, site, logger)
+        except IndexError:
+            # ignore multiline errors
+            pass
 
 nginx()
